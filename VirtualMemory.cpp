de49de0fe93f1cbb19c109a9592e1af9248959ba
ronatene <ronnie.tene@mail.huji.ac.il>
2025-06-09 14:32:15 +0300

    virtual memory

---
+++ VirtualMemory.cpp
@@ -0,0 +1,15 @@
+#include "PhysicalMemory.h"
+#include "VirtualMemory.h"
+
+
+void VMinitialize() {
+    // Initialize the virtual memory
+}
+
+
+int VMread(uint64_t virtualAddress, word_t* value){
+}
+
+
+int VMwrite(uint64_t virtualAddress, word_t value){
+}
